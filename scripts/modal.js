f612b84c9c4d2a6e6ccdcae2cf18f4c3e52a427b
Joao Borchoski <joaohenriqueborchoski>
2022-09-21 16:49:55 -0300

    entrega

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,16 @@
-/* Desenvolva seu código aqui... */
+const butEsqueceu = document.getElementById("butEsqueceu")
+const butRecuperar = document.getElementById("butRecuperar")
+const butX = document.getElementById("butX")
+
+butEsqueceu.addEventListener("click", function(){
+    document.getElementById("modalTotal").classList.add("modal")
+})
+
+butRecuperar.addEventListener("click", function(){
+    alert("Email enviado")
+    document.getElementById("modalTotal").classList.remove("modal")
+})
+
+butX.addEventListener("click", function(){
+    document.getElementById("modalTotal").classList.remove("modal")
+})
